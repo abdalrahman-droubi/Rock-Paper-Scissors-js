26d0f0e936751829ac831ead8357f1ef7162f653
abdalrahman-droubi <abdalrahman.drobi@outlook.com>
2023-09-26 15:54:51 +0300

    1

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const computerChoices = ["Rock", "Paper", "Scissors"];
 const ChooseSection = document.getElementById("ChooseSection");
+const resultSection = document.getElementById("resultSection");
 const resultSectionTitle = document.getElementById("resultSectionTitle");
 const resultSectionImg1 = document.getElementById("resultSectionImg1");
 const resultSectionImg2 = document.getElementById("resultSectionImg2");
-const resultSection = document.getElementById("resultSection");
 const againBtn = document.getElementById("againBtn");
 
-function handleResult(userChoice) {
+const handleResult = (userChoice) => {
+  const computerChoice = computerChoices[Math.floor(Math.random() * 3)];
   ChooseSection.style.display = "none";
   resultSection.style.display = "block";
   againBtn.style.display = "block";
-  let computerChoice = computerChoices[Math.floor(Math.random() * 3)];
   resultSectionImg1.src = `./assets/${userChoice}.svg`;
   resultSectionImg2.src = `./assets/${computerChoice}.svg`;
   resultSectionImg2.style.transform = "scaleX(-1)";
   if (
-    (computerChoice == "Rock" && userChoice == "Paper") ||
-    (computerChoice == "Scissors" && userChoice == "Rock") ||
-    (computerChoice == "Paper" && userChoice == "Scissors")
+    (computerChoice === "Rock" && userChoice === "Paper") ||
+    (computerChoice === "Scissors" && userChoice === "Rock") ||
+    (computerChoice === "Paper" && userChoice === "Scissors")
   ) {
     resultSectionTitle.textContent = "YOU WIN!";
   } else if (
-    (computerChoice == "Paper" && userChoice == "Rock") ||
-    (computerChoice == "Rock" && userChoice == "Scissors") ||
-    (computerChoice == "Scissors" && userChoice == "Paper")
+    (computerChoice === "Paper" && userChoice === "Rock") ||
+    (computerChoice === "Rock" && userChoice === "Scissors") ||
+    (computerChoice === "Scissors" && userChoice === "Paper")
   ) {
     resultSectionTitle.textContent = `YOU LOST!`;
   } else {
     resultSectionTitle.textContent = `YOU TIED!`;
   }
-}
+};
 
 againBtn.addEventListener("click", () => {
   ChooseSection.style.display = "block";
